d9daed31166015ddb4ebeda0835ee062dcf55067
Your Name <noureddinelegragui@gmail.com>
2020-06-08 19:22:14 +0100

    contentful

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ export default function Title({ title, center }) {
   return (
     <TitleWrapper className="row" center={center}>
       <div className="col">
-        <h2 classNamr="text-title">{title}</h2>
+        <h2 className="text-title">{title}</h2>
         <div className="title-underline" />
       </div>
     </TitleWrapper>
--- src/context/context.js
+++ src/context/context.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { linkData } from "./linkData";
 import { socialData } from "./socialData";
-import { items } from "./productData";
-
+//import { items } from "./productData";
+import { client } from "./contentful";
 const ProductContext = React.createContext();
 //Provider
 //Consummer
@@ -31,8 +31,14 @@ class ProductProvider extends Component {
   };
   componentDidMount() {
     //from contentful items
+    //  this.setProducts(items);
+    client
+      .getEntries({
+        content_type: "btsProducts",
+      })
 
-    this.setProducts(items);
+      .then((response) => this.setProducts(response.items))
+      .catch(console.error);
   }
 
   //set products
